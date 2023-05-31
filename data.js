94d649af8f9ca8122629653f391eff5f971a90f2
Valentina Lopez <valentinalopezh98@gmail.com>
2023-05-30 23:24:25 -0300

    add data.js

---
+++ data.js
@@ -0,0 +1,227 @@
+const productsData = [
+    {
+        id: 1,
+        name: "Melodrama",
+        artist: "Lorde",
+        year: "2017",
+        rate: 8.8,
+        price: 25000,
+        img: "img/1.png"
+    },
+    {
+        id: 2,
+        name: "Fetch the Bolt Cutters",
+        artist: "Fiona Apple",
+        year: "2020",
+        rate: 10,
+        price: 24990,
+        img: 
+    },
+    {
+        id: 3,
+        name: "Post",
+        artist: "Björk",
+        year: "1995",
+        rate: 10,
+        price: 21990,
+        img: 
+    },
+    {
+        id: 4,
+        name: "Locura",
+        artist: "Virus",
+        year: "1985",
+        rate: 9.5,
+        price: 15000,
+        img: 
+    },
+    {
+        id: 5,
+        name: "Graduation",
+        artist: "Kanye West",
+        year: "2007",
+        rate: 8.7,
+        price: 27990,
+        img: 
+    },
+    {
+        id: 6,
+        name: "Flower Boy",
+        artist: "Tyler, The Creator",
+        year: "2017",
+        rate: 8.5,
+        price: 19990,
+        img: 
+    },
+    {
+        id: 7,
+        name: "Portamento",
+        artist: "The Drums",
+        year: "2011",
+        rate: 7.4,
+        price: 21990,
+        img: 
+    },
+    {
+        id: 8,
+        name: "Tidal",
+        artist: "Fiona Apple",
+        year: "1996",
+        rate: 9,
+        price: 25500,
+        img: 
+    },
+    {
+        id: 9,
+        name: "Pop 2",
+        artist: "Charli XCX",
+        year: "2017",
+        rate: 8.4,
+        price: 23990,
+        img: 
+    },
+    {
+        id: 10,
+        name: " Synchronicity",
+        artist: "The Police",
+        year: "1983",
+        rate: 7.8,
+        price: 23600,
+        img: 
+    },
+    {
+        id: 11,
+        name: "Experimental Jet Set, Trash and No Star",
+        artist: "Sonic Youth",
+        year: "1994",
+        rate: 6.7,
+        price: 26000,
+        img: 
+    },
+    {
+        id: 12,
+        name: "90125",
+        artist: "Yes",
+        year: 1983,
+        rate: 7.2,
+        price: 24990,
+        img: 
+    },
+    {
+        id: 13,
+        name: "Hot Space",
+        artist: "Queen",
+        year: "1982",
+        rate: 7.3,
+        price: 29990,
+        img: 
+    },
+    {
+        id: 14,
+        name: "Sgt. Pepper's Lonely Hearts Club Band",
+        artist: "The Beatles",
+        year: "1967",
+        rate: 10,
+        price: 35990,
+        img: 
+    },
+    {
+        id: 15,
+        name: "Pet Sounds",
+        artist: "The Beach Boys",
+        year: "1966",
+        rate: 9.4,
+        price: 34990,
+        img: 
+    },
+    {
+        id: 16,
+        name: "Yeezus",
+        artist: "Kanye West",
+        year: 
+        rate:
+        price: 
+        img: 
+    },
+    {
+        id: 17,
+        name: "Paul's Boutique",
+        artist: "Beastie Boys",
+        year: "1989",
+        rate: 9.5,
+        price: 32990,
+        img: 
+    },
+    {
+        id: 18,
+        name:
+        artist:
+        year:
+        rate:
+        price: 
+        img: 
+    },
+    {
+        id: 19,
+        name:
+        artist:
+        year:
+        rate:
+        price: 
+        img: 
+    },
+    {
+        id: 20,
+        name:
+        artist:
+        year:
+        rate:
+        price: 
+        img: 
+    },
+    {
+        id: 21,
+        name:
+        artist:
+        year:
+        rate:
+        price: 
+        img: 
+    },
+    {
+        id: 22,
+        name:
+        artist:
+        year:
+        rate:
+        price: 
+        img: 
+    },
+    {
+        id: 23,
+        name:
+        artist:
+        year:
+        rate:
+        price: 
+        img: 
+    },
+    {
+        id: 24,
+        name:
+        artist:
+        year:
+        rate:
+        price: 
+        img: 
+    },
+    {
+        id: 25,
+        name:
+        artist:
+        year:
+        rate:
+        price: 
+        img: 
+    }
+]
